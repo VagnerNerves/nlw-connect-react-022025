70164eac01aade2bc40a9e6c032972be6cfa95c9
Vagner Nerves <vagnernervessantos@gmail.com>
2025-02-18 18:50:12 -0300

    feat: Add inviteLink Props and ajusted for copy Invite Link.

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,19 +1,26 @@
+'use client'
+
 import { Copy, Link } from 'lucide-react'
 
 import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 
-export function InviteLinkInput() {
+type InviteLinkInputProps = {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
-      <InputField
-        readOnly
-        defaultValue="http://localhost:3000/invite/3291381203801293812313"
-      />
-      <IconButton className="-mr-2">
+      <InputField readOnly defaultValue={inviteLink} />
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
