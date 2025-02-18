73e4e7fc9fa4c9fde70b5154eef0aba55c2d3d24
Vagner Nerves <vagnernervessantos@gmail.com>
2025-02-18 18:31:03 -0300

    feat: Add components on page invite.

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,14 +1,10 @@
 import Image from 'next/image'
 
-import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
-
 import logo from '../../assets/logo.svg'
-import medalCooper from '../../assets/medal-cooper.svg'
-import medalGold from '../../assets/medal-gold.svg'
-import medalSilver from '../../assets/medal-silver.svg'
 
-import { IconButton } from '@/components/icon-button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { InviteLinkInput } from './invite-link-input'
+import { Ranking } from './ranking'
+import { Stats } from './status'
 
 export default function InvitePage() {
   return (
@@ -36,92 +32,12 @@ export default function InvitePage() {
           </div>
         </div>
 
-        <InputRoot>
-          <InputIcon>
-            <Link className="size-5" />
-          </InputIcon>
-          <InputField
-            readOnly
-            defaultValue="http://localhost:3000/invite/3291381203801293812313"
-          />
-          <IconButton className="-mr-2">
-            <Copy className="size-5" />
-          </IconButton>
-        </InputRoot>
+        <InviteLinkInput />
 
-        <div className="grid md:grid-cols-3 gap-3">
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1042
-            </span>
-            <span className="text-sm text-gray-300 leading-none text-center">
-              Acessos ao link
-            </span>
-            <MousePointerClick className="size-5 text-purple top-3 left-3 absolute" />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1042
-            </span>
-            <span className="text-sm text-gray-300 leading-none text-center">
-              Inscrições feitas
-            </span>
-            <BadgeCheck className="size-5 text-purple top-3 left-3 absolute" />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              3&ordm;
-            </span>
-            <span className="text-sm text-gray-300 leading-none text-center">
-              Posição no ranking
-            </span>
-            <Medal className="size-5 text-purple top-3 left-3 absolute" />
-          </div>
-        </div>
+        <Stats />
       </div>
 
-      <div className="w-full max-w-[440px] space-y-5">
-        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-          Ranking de indicações
-        </h2>
-        <div className="space-y-4">
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1º</span> | Vagner Nerves
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image src={medalGold} alt="" className="absolute top-0 right-8" />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">2º</span> | Vagner Nerves
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image
-              src={medalSilver}
-              alt=""
-              className="absolute top-0 right-8"
-            />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">3º</span> | Vagner Nerves
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image
-              src={medalCooper}
-              alt=""
-              className="absolute top-0 right-8"
-            />
-          </div>
-        </div>
-      </div>
+      <Ranking />
     </div>
   )
 }
